1806a7a418e5e58639cfb011a5075079e42c341c
Heta Jhaveri <heta26jhaveri@gmail.com>
2018-11-24 13:41:34 -0500

    Update ProductDiscountedBill.java

--- ProductDiscountedBill.java
+++ ProductDiscountedBill.java
@@ -1,10 +1,10 @@
 package discountsystem.core;
-
+/**
+	This is the product discount implementation of discounted bill
+*/
 public class ProductDiscountedBill implements DiscountedBill{
-
 	@Override
 	public double getDiscountedBill(double productBill, MemeberyShipTypes membershipType){
 		return productBill - productBill*membershipType.getProductDiscount();
 	}
-
 }
