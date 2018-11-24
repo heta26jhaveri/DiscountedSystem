9fa23eec10e2dacfeac5da21d370666b115aedd7
Heta Jhaveri <heta26jhaveri@gmail.com>
2018-11-24 13:39:20 -0500

    Update DiscountedBill.java

--- DiscountedBill.java
+++ DiscountedBill.java
@@ -1,5 +1,8 @@
 package discountsystem.core;
 
+/**
+	An interface to defines discounted bill based on membership
+*/
 public interface DiscountedBill{
 	public double getDiscountedBill(double bill, MemeberyShipTypes membershipType);
 }
