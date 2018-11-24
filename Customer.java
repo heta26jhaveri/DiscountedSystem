060c9d363a5c41e4b947e29521a775a9236abeba
Heta Jhaveri <heta26jhaveri@gmail.com>
2018-11-24 13:38:27 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,17 +1,23 @@
 package discountsystem.core;
 
 /**
- * This class defines the customer
- * @author apple
+ * This class defines the customer - name and his/her membership type
+ * @author jhaverih
  *
  */
 public class Customer{
 	private String name;
+	private MemeberyShipTypes memberType;
 	
+	public Customer(String name, MemeberyShipTypes memberType){
+		this.name = name;
+		this.memberType = memberType;
+	}
+		
 	public MemeberyShipTypes getMemberType(){
 		return memberType;
 	}
-
+	
 	public void setMemberType(MemeberyShipTypes memberType){
 		this.memberType = memberType;
 	}
@@ -19,17 +25,11 @@ public class Customer{
 	public String getName(){
 		return name;
 	}
-
-	private MemeberyShipTypes memberType;
 	
-	public Customer(String name, MemeberyShipTypes memberType){
-		this.name = name;
-		this.memberType = memberType;
-	}
-	
+	/**
+		Check if the customer is a member of the saloon
+	*/
 	public boolean isMember(){
 		return !this.memberType.equals(MemeberyShipTypes.NONE);
 	}
-	
-	
 }
