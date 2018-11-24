88aad874ae9281a82941f3eea83e8679e0134a9e
Heta Jhaveri <heta26jhaveri@gmail.com>
2018-11-24 13:42:03 -0500

    Update ServiceDiscountedBill.java

--- ServiceDiscountedBill.java
+++ ServiceDiscountedBill.java
@@ -1,10 +1,12 @@
 package discountsystem.core;
 
+/**
+	This is the service discount implementation of discounted bill
+*/
 public class ServiceDiscountedBill implements DiscountedBill{
 
 	@Override
 	public double getDiscountedBill(double serviceBill, MemeberyShipTypes memberType){
 		return serviceBill - (serviceBill*memberType.getServiceDiscount());
 	}
-	
 }
